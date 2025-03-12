f6ba23cbd28d827d231c0ead4d9c457c5c0dcfa6
kevinrasmusson <kevin.rasmusson.03@gmail.com>
2025-03-12 16:06:03 +0100

    Made some cool changes to the main.js file

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ function fetchJSONData() {
     .then((json) => {
       console.log('JSON data fetched:', json);
       jsonData = json; // Store the JSON data in a global variable
-      parseJSONData(json);
+      //parseJSONData(json);
     })
     .catch((error) => console.log(error));
 }
@@ -49,8 +49,9 @@ function parseJSONData(json, searchInput = '') {
   json.forEach(item => {
     const div = document.createElement('div');
     const kommun = item.Kommun;
-    const styre = item.Styre.replace(/,/g,'');
-    div.innerHTML = `Kommun: ${kommun}<br>Styre: ${styre}`;
+    const styre = item.Styre;
+    const parties = filterParties(styre);
+    div.innerHTML = `Kommun: ${kommun}<br>Styre: ${parties.join(', ')}`;
     resultsContainer.appendChild(div);
   });
 }
@@ -59,4 +60,42 @@ function filterResults(searchTerm) {
   const filteredData = jsonData.filter(item => item.Kommun && item.Kommun.toLowerCase().includes(searchTerm));
   parseJSONData(filteredData, searchTerm);
 }
-function lettersToPartyName(styre){}
+function filterParties(styre){
+  const parties = [];
+  const partyArray = styre.split(',').filter(party => party !== '');
+  partyArray.forEach(party => {
+    if(party !== ''){
+      const partyName = letterToPartyName(party);
+      parties.push(partyName);
+    }
+  });
+  return parties;
+}
+function letterToPartyName(party){
+  switch(party){
+    case 'M':
+      return 'Moderaterna';
+    case 'C':
+      return 'Centerpartiet';
+    case 'L':
+      return 'Liberalerna';
+    case 'KD':
+      return 'Kristdemokraterna';
+    case 'S':
+      return 'Socialdemokraterna';  
+    case 'V':
+      return 'Vänsterpartiet';
+    case 'MP':
+      return 'Miljöpartiet';
+    case 'SD':
+      return 'Sverigedemokraterna';
+    case 'FI':
+      return 'Feministiskt initiativ';
+    case 'ÖP':
+      return 'Övriga';
+    default:
+      return party;
+  }
+
+
+}
